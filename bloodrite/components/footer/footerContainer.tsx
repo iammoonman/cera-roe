bcb9e7e77398687dfdb0a848b034606350aca2d0
iammoonman <420saas@gmail.com>
2023-09-21 10:55:56 -0400

    Remove old Bloodrite

--- bloodrite/components/footer/footerContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const FooterContainer = () => {
-	return (
-		<footer className="w-full flex flex-wrap justify-between mt-auto">
-			<div className="text-gray-600 text-center text-sm w-full bg-black">Wizards of the Coast, Magic: The Gathering, and their logos and symbols are trademarks of Wizards of the Coast LLC. Discord and its logos and symbols are trademarks of Discord Inc. This site is not affiliated with those companies. The banner image was created by Alayna Danner.</div>
-		</footer>
-	);
-};
-export default FooterContainer;
